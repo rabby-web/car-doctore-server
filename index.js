25d8b7ac6dfe4e3c8e889cb87f8e8695bd625a86
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-26 17:10:05 +0600

    booking show data

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
 
       const options = {
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
 
       const result = await serviceCollection.findOne(query, options);
@@ -49,9 +49,18 @@ async function run() {
     });
 
     // bookings
-    app.post("/booking", async (req, res) => {
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
-      console.log(booking);
+      console.log("bookings", booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
