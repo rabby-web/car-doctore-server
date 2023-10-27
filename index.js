e46200ca50ae554a284afbf6ccbf6b9f1dc13d65
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-27 06:57:15 +0600

    booking update

--- index.js
+++ index.js
@@ -65,6 +65,20 @@ async function run() {
       res.send(result);
     });
 
+    // update
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // booking delete
     app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
