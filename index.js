533e4d0be3f5c496cc6d74b93990ab59e28535b4
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-31 12:47:39 +0600

    tooken add

--- index.js
+++ index.js
@@ -30,6 +30,30 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+const logger = async (req, res, next) => {
+  console.log("called:", req.host, req.originalUrl);
+  next();
+};
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of", token);
+  if (!token) {
+    return res.status(401).send({ message: "forbidden" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // err
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: "unauthorized" });
+    }
+    // decoded
+    console.log("/value in the token", decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -39,7 +63,7 @@ async function run() {
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
     // auth related API
-    app.post("/jwt", async (req, res) => {
+    app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -55,7 +79,7 @@ async function run() {
     });
 
     // services related API
-    app.get("/services", async (req, res) => {
+    app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -74,8 +98,13 @@ async function run() {
     });
 
     // bookings
-    app.get("/bookings", async (req, res) => {
-      console.log("token", req.cookies.token);
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
+      // console.log("token", req.cookies.token);
+      console.log(req.query.email);
+      console.log("from value", req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
